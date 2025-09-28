4cab8536ce5553c40ba1c54d17e7863191427b3f
Raymond <radd254@users.noreply.github.com>
2025-09-28 23:37:06 +0700

    fix thu tu tao bang trong file sql

--- exe201(data).sql
+++ exe201(data).sql
@@ -24,9 +24,104 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `UserToken` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `userNumber` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `PASSWORD` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Role` enum('Admin','Customer','CustomerPre') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'Customer',
+  `Score` int DEFAULT '0',
+  `FailedLoginAttempts` int DEFAULT '0',
+  `STATUS` enum('Active','Locked') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Active',
+  `isFirstLogin` tinyint(1) NOT NULL DEFAULT '1',
+  `LockTimestamp` datetime DEFAULT NULL,
+  `avatar` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserToken`, `Username`, `userNumber`, `PASSWORD`, `Role`, `Score`, `FailedLoginAttempts`, `STATUS`, `isFirstLogin`, `LockTimestamp`, `avatar`) VALUES
+('1', 'Phạm Xuân Dương', NULL, '$2y$10$B7a6ZAuaWD3FfAlchcU7zOLY.mbefvs1sGn61qzfun1vnCtgMjRee', 'CustomerPre', 888, 0, 'Active', 0, NULL, 'avatar/avatar.png'),
+('2', 'Hello world', NULL, '$2y$10$czoqxWa.7BspACtVcphEG.lWTIr1xCsjHxFsYHK2Hib5Ctmxq1mZa', 'Customer', 0, 0, 'Active', 0, NULL, 'avatar/avatar.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `museum`
+--
+
+DROP TABLE IF EXISTS `museum`;
+CREATE TABLE `museum` (
+  `MuseumID` int NOT NULL,
+  `MuseumName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Description` text COLLATE utf8mb4_unicode_ci,
+  `Latitude` decimal(10,7) NOT NULL,
+  `Longitude` decimal(10,7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `museum`
+--
+
+INSERT INTO `museum` (`MuseumID`, `MuseumName`, `Address`, `Description`, `Latitude`, `Longitude`) VALUES
+(3, 'Bảo tàng Lịch Sử Quân sự Việt Nam', 'Km6+500, CT03, Xuân Phương, Hà Nội', 'Bảo tàng Lịch sử Quân sự Việt Nam là một trong 6 bảo tàng cấp quốc gia và là bảo tàng đầu hệ của hệ thống bảo tàng quân đội, được thành lập ngày 17/7/1956, địa chỉ tại số 28A đường Điện Biên Phủ, quận Ba Đình, thành phố Hà Nội. Bảo tàng hiện đang trưng bày và lưu giữ hàng vạn hiện vật, trong đó có 4 bảo vật quốc gia, bao gồm: (Máy bay MiG-21 số hiệu 4324; Máy bay MiG-21 số hiệu 5121, Tấm Bản đồ Quyết tâm chiến dịch Hồ Chí Minh; Xe tăng T-54B số hiệu 843).\r\n\r\nDưới sự lãnh đạo, chỉ đạo của Quân ủy Trung ương, Bộ Quốc phòng, trực tiếp là Thủ trưởng Tổng cục Chính trị, Bảo tàng LSQS Việt Nam từng bước phát triển cả về cơ cấu tổ chức cũng như chức năng, nhiệm vụ làm tốt vai trò của một thiết chế văn hoá, một bộ phận quan trọng trong hệ thống hoạt động công tác đảng, công tác chính trị của Quân đội nhân dân Việt Nam.\r\n\r\nThực hiện sắc lệnh số 65/SL-TN, ngày 23/11/1945 của Chủ tịch Hồ Chí Minh "Về bảo tồn di sản văn hóa", ngay trong giai đoạn kháng chiến chống thực dân Pháp (1945 - 1954), Bộ Quốc phòng đã chỉ đạo các cơ quan, đơn vị chú trọng thu thập, lưu giữ các tài liệu, hình ảnh, hiện vật phản ánh cuộc kháng chiến của nhân dân ta dưới sự lãnh đạo của Đảng, đứng đầu là Chủ tịch Hồ Chí Minh, nhằm phục vụ sự nghiệp kháng chiến và kiến quốc. Trên cơ sở đó, cuối năm 1954, Tổng Quân ủy (nay là Quân ủy Trung ương) đã có chủ trương xây dựng Bảo tàng Quân đội.\r\n\r\nLúc mới thành lập, Ban xây dựng Bảo tàng Quân đội có 13 người với nhiệm vụ "Nghiên cứu sưu tầm các tài liệu hiện vật, di tích lịch sử thuộc về quân đội; tiến hành thu thập, sắp xếp các tài liệu, hiện vật cho có hệ thống; tổ chức bảo quản các tài liệu hiện vật lịch sử, đồng thời liên hệ với các địa phương để hướng dẫn bảo quản những di tích lịch sử của quân đội ở địa phương; nghiên cứu kế hoạch và thực hiện trình bày, tổ chức nên Bảo tàng Quân đội".\r\n\r\nNgay sau khi thành lập, Ban Xây dựng Bảo tàng Quân đội bắt tay vào soạn thảo các văn bản về công tác bảo tồn, bảo tàng giúp Tổng cục Chính trị chỉ đạo toàn quân sưu tầm tài liệu, hiện vật vừa để xây dựng phòng truyền thống của đơn vị, vừa đóng góp hiện vật cho Bảo tàng. Với tinh thần làm việc khẩn trương, được sự chỉ đạo sát sao của Thủ trưởng Tổng cục Chính trị, sau hơn 3 năm tiến hành xây dựng và trưng bày, ngày 21/12/1959, nhân kỷ niệm 15 năm ngày thành lập Quân đội nhân dân Việt Nam (22/12/1944 - 22/12/1959), Bảo tàng Quân đội chính thức mở cửa phục vụ khách tham quan. Tới duyệt nội dung trưng bày bảo tàng trước khi mở cửa đón khách tham quan, Chủ tịch Hồ Chí Minh đã nói: "Bảo tàng Quân đội là một cuốn sử sống có tác dụng rất to lớn đến việc giáo dục truyền thống tốt đẹp của dân tộc ta cho người Việt Nam, nhất là thế hệ trẻ. Bảo tàng Quân đội còn giúp khách nước ngoài hiểu rõ về Việt Nam ta, về cuộc đấu tranh chính nghĩa của nhân dân ta. Vì vậy, Bảo tàng phải tạo điều kiện tốt nhất để cho khách đến tham quan".\r\n\r\nGhi sổ vàng lưu niệm khi tới dư Lễ khánh thành Bảo tàng Quân đội, Thủ tướng Phạm Văn Đồng viết: "Bảo tàng Quân đội nhắc lại lịch sử vẻ vang của Quân đội nhân dân Việt Nam ta, một quân đội anh hùng của một dân tộc anh hùng. Bảo tàng Quân đội là một trường học và là nguồn phấn khởi đối với người xem, đối với nhân dân ta và Quân đội ta".\r\n\r\nSau khi hoàn thành xuất sắc nhiệm vụ xây dựng và khai trương bảo tàng, Ban Xây dựng Bảo tàng Quân đội chuyển thành Phòng Bảo tàng quân đội trực thuộc Cục Tuyên huấn/ Tổng cục Chính trị với nhiệm vụ "Đón tiếp, hướng dẫn khách tham quan, bảo vệ, bảo quản hiện vật, kho tàng, tu bổ sửa chữa nhỏ hệ thống trưng bày".\r\n\r\nSau gần 5 năm mở cửa đón khách tham quan, ngày 15/5/1964, Tổng cục Chính trị ra Quyết định nâng cấp Phòng Bảo tàng Quân đội thành Viện Bảo tàng Quân đội trực thuộc Tổng cục Chính trị. Viện có nhiệm vụ: Nghiên cứu, sưu tầm hiện vật và tuyên truyền giáo dục truyền thống quân đội; thực hiện công tác nghiên cứu khoa học; giúp Tổng cục Chính trị chỉ đạo hoạt động bảo tàng, nhà truyền thống toàn quân. Đây là một bước ngoặt đánh dấu sự trưởng thành của Bảo tàng Quân đội, nâng vị thế bảo tàng lên một tầm cao mới.\r\n\r\nLúc này, cuộc kháng chiến chống Mỹ cứu nước diễn ra ngày càng ác liệt, không chỉ ở chiến trường miền Nam, đế quốc Mỹ đã leo thang ra cả miền Bắc. Vì vậy, tổ chức biên chế, chức năng nhiệm vụ và hoạt động của Viện Bảo tàng Quân đội cũng có điều chỉnh cho phù hợp với điều kiện cả nước có chiến tranh. Nhiều cán bộ Viện Bảo tàng Quân đội đã trực tiếp dấn thân vào cuộc chiến đấu, theo sát bộ đội trên các chiến trường để sưu tầm hiện vật, có đồng chí đã anh dũng hy sinh trong khi làm nhiệm vụ, tiêu biểu như liệt sĩ Dương Quang Chính. Trong 10 năm, từ 1965 đến năm 1975, Viện Bảo tàng Quân đội đã phục vụ tham quan cho gần 2 triệu khách trong và ngoài nước thuộc hơn 80 quốc gia trên thế giới.\r\n\r\nSau khi đất nước thống nhất, cùng với sự hồi sinh, phát triển của đất nước, của quân đội, Viện Bảo tàng Quân đội cũng có bước trưởng thành mới. Năm 1987, Phân viện Điện Biên Phủ trên cơ sở tiếp nhận Khu di tích Điện Biên Phủ được thành lập. Năm 1988, sáp nhập Xưởng Mỹ thuật Quân đội vào Viện Bảo tàng. Tháng 7/1994, Bộ Văn hoá Thông tin (nay là Bộ Văn hoá, Thể thao và Du lịch) công nhận Viện Bảo tàng Quân đội là bảo tàng cấp quốc gia. Cơ cấu tổ chức của Viện được điều chỉnh cho phù hợp với chức năng nhiệm vụ mới.\r\n\r\nĐến ngày 4/12/2002, Thủ tướng Chính phủ ra quyết định số 1155/QĐ-TTg đổi tên Viện Bảo tàng Quân đội thành Bảo tàng Lịch sử Quân sự Việt Nam. Với việc đổi tên, vai trò, chức năng, nhiệm vụ cũng như vị thế của Bảo tàng được nâng lên ngang tầm với các bảo tàng quốc gia; quy mô, phạm vi trưng bày được mở rộng. Được sự quan tâm của Đảng, Nhà nước, sự lãnh đạo, chỉ đạo của Quân ủy Trung ương, Bộ Quốc phòng, trực tiếp là Tổng cục Chính trị, sự giúp đỡ của các đơn vị trong toàn quân cùng sự nỗ lực, cố gắng của các cán bộ, nhân viên bảo tàng. Trong suốt quá trình xây dựng và phát triển, Bảo tàng Lịch sử Quân sự Việt Nam đã hoạt động xứng đáng với tầm vóc của một thiết chế văn hóa của xã hội, một bộ phận quan trọng trong hệ thống công tác đảng, công tác chính trị của Quân đội nhân dân Việt Nam. Bảo tàng Lịch sử Quân sự Việt Nam trở thành một trong 6 bảo tàng quốc gia và là bảo tàng đầu hệ của hệ thống bảo tàng quân đội, hàng năm tiếp đón và phục vụ hàng chục vạn lượt khách trong và ngoài nước tới tham quan, học tập.\r\n\r\nVới những kết quả đạt được, Bảo tàng Lịch sử Quân sự Việt Nam đã được Nhà nước tặng thưởng nhiều phần thưởng cao quý: Huân chương Quân công hạng Hai (1984), Huân chương chiến công hạng Nhất (1970), Huân chương chiến công hạng Hai (1961), 2 Huân chương chiến công hạng Ba (1985, 2004), Huân chương bảo vệ Tổ quốc hạng Nhì (2010), Huân chương bảo vệ Tổ quốc hạng Nhất (2016). Bảo tàng còn được Nhà nước Cộng hoà dân chủ nhân dân Lào tặng Huân chương Lao động hạng Ba (2005), Huân chương Anh dũng hạng Hai (2008), được Bộ văn hóa – Thể thao và Du lịch tặng Cờ (năm 2015). Ngày 15/7/2016, tại Lễ kỷ niệm 60 năm Ngày truyền thống Bảo tàng Lịch sử Quân sự Việt Nam (17/7/1956 - 17/7/2016), Bảo tàng vinh dự đón nhận Huân chương Bảo vệ Tổ quốc hạng Nhất. Đây là sự ghi nhận của Đảng, Nhà nước, Quân đội đối với biết bao thế hệ cán bộ, nhân viên của Bảo tàng, với lòng say mê nghề nghiệp, sự trân trọng đối với lịch sử, không tiếc mồ hôi, công sức và cả xương máu, vừa làm vừa học, từng bước xây những viên gạch đầu tiên cho truyền thống đoàn kết, chủ động, kiên trì, sáng tạo của Bảo tàng Lịch sử Quân sự Việt Nam.\r\n\r\nTrong xu thế thế đổi mới ngày nay, nhiệm vụ của Bảo tàng LSQS Việt Nam hết sức nặng nề. Bảo tàng vừa phải tiếp tục củng cố nâng cấp hệ thống trưng bày tại Cột Cờ phục vụ khách tham quan, vừa từng bước triển khai thực hiện Dự án xây dựng Bảo tàng LSQS Việt Nam mới với quy mô lớn theo hướng hiện đại như chủ trương của Quân uỷ Trung ương và Bộ Quốc phòng: "Bảo tàng LSQS Việt Nam là một thiết chế đa năng tổng hợp, đặc biệt quan trọng, tiêu biểu của Thủ đô Hà Nội và Quốc gia, phản ánh toàn diện, sâu sắc, có hệ thống, đa dạng, phong phú hài hòa và hấp dẫn LSQS Việt Nam qua các thời đại".\r\n\r\nĐể làm được việc này, cán bộ, chiến sĩ, nhân viên Bảo tàng LSQS Việt Nam phải nỗ lực phấn đấu nhiều hơn nữa, không ngừng rèn luyện, học tập nâng cao trình độ, năng lực chuyên môn, phẩm chất đạo đức, hoàn thành tốt nhiệm vụ, chức trách được giao. Xứng đáng là Bảo tàng đầu hệ của hệ thống Bảo tàng Quân đội và là một trong 6 bảo tàng Quốc gia./.\r\n\r\nNguồn: Bảo tàng Lịch sử Quân sự Việt Nam ', 21.0086136, 105.7556136),
+(4, 'Bảo tàng Hồ Chí Minh', '19 P. Ngọc Hà, Đội Cấn, Ba Đình, Hà Nội 100000, Việt Nam', 'Bảo tàng Hồ Chí Minh là bảo tàng ở Hà Nội tập trung chủ yếu vào việc trưng bày những hiện vật, tư liệu về cuộc đời và con người Hồ Chí Minh. Nằm trong khu vực có nhiều di tích như: Lăng Chủ tịch Hồ Chí Minh, Khu di tích Phủ Chủ tịch, Chùa Một Cột,... tạo thành một quần thể các di tích thu hút khách tham quan trong và ngoài nước. Bảo tàng tọa lạc tại số 19 phố Ngọc Hà, phường Đội Cấn, quận Ba Đình, thành phố Hà Nội, nằm phía sau Lăng Chủ tịch Hồ Chí Minh và cạnh công viên Bách Thảo. Bảo tàng Hồ Chí Minh là bảo tàng lớn và hiện đại nhất Việt Nam.', 21.0372081, 105.8330520),
+(5, 'Bảo tàng Dân tộc học', 'Đ. Nguyễn Văn Huyên, Quan Hoa, Cầu Giấy, Hà Nội', 'Ngày 12 tháng 11 năm 1997, tại Hà Nội diễn ra một sự kiện quan trọng: Phó Chủ tịch nước Cộng hòa xã hội chủ nghĩa Việt Nam Nguyễn Thị Bình và Tổng thống Cộng hòa Pháp Jacques Chirac cắt băng khai trương Bảo tàng Dân tộc học Việt Nam.\r\n\r\nTừ tòa Trống đồng giới thiệu 54 dân tộc Việt Nam, Bảo tàng Dân tộc học Việt Nam đã từng bước hoàn thiện khu Vườn Kiến trúc với 10 công trình dân gian đại diện cho các loại hình khác nhau của nhiều dân tộc và vùng văn hóa. Không dừng lại ở giới thiệu về Việt Nam, Bảo tàng xây dựng tòa Cánh diều, trưng bày kết nối với các tộc người ở Đông Nam Á. Và xa hơn thế, các trưng bày vươn ra châu Á, châu Đại Dương, châu Phi và Mỹ Latin, nhờ những sưu tập hiện vật được hiến tặng. Trải qua hành trình hơn 20 năm, cùng với các trưng bày thường xuyên là hàng loạt trưng bày nhất thời, những hoạt động trình diễn văn hóa phi vật thể, các chương trình hoạt động giáo dục trải nghiệm… đã làm cho Bảo tàng Dân tộc học Việt Nam sống động và trở thành một điểm sáng, một điểm tham quan thu hút đông đảo du khách trong nước và quốc tế, được công chúng mến mộ. Trong ba năm liền (2012, 2013, 2014), Bảo tàng Dân tộc học Việt Nam được TripAdvisor, trang web du lịch nổi tiếng thế giới, bình chọn là Bảo tàng xuất sắc, xếp thứ tư trong 25 bảo tàng hấp dẫn nhất châu Á. Ba năm tiếp theo (2015, 2016, 2017), Bảo tàng Dân tộc học Việt Nam được vinh danh là Điểm tham quan du lịch hàng đầu Việt Nam, do Bộ Văn hóa, Thể thao và Du lịch, Tổng cục Du lịch và Hiệp hội Du lịch Việt Nam trao tặng. Thậm chí, ngay trong thời kỳ dịch bệnh Covid-19 ảnh hưởng nặng nề đến Việt Nam và các nước trên thế giới (2020-2021), Bảo tàng Dân tộc học Việt Nam vẫn không ngừng sáng tạo để đưa đến cho công chúng những sản phẩm văn hóa đa dạng, đặc sắc và thích ứng linh hoạt với nhu cầu thay đổi của xã hội, cũng như tình hình "bình thường mới". Bảo tàng Dân tộc học Việt Nam đã vinh hạnh và tự hào khi là bảo tàng duy nhất ở Việt Nam được Bộ Văn hóa, Thể thao và Du lịch trao tặng Bằng khen vì đã có thành tích xuất sắc trong xây dựng và tổ chức hoạt động du lịch tại địa phương năm 2021.\r\n\r\nĐể đạt được các kết quả đó, trong suốt quá trình hình thành và phát triển, đội ngũ nhân viên của Bảo tàng Dân tộc học Việt Nam luôn hướng theo các quan niệm, tiếp cận phương thức hoạt động mới. Bảo tàng Dân tộc học Việt Nam cũng luôn nhận được sự hỗ trợ có hiệu quả của nhiều chuyên gia, tổ chức trong nước và quốc tế. Quá trình làm việc không mệt mỏi ấy là quá trình cán bộ, nhân viên Bảo tàng tích lũy kiến thức và những trải nghiệm chuyên nghiệp quý báu.', 21.0405575, 105.7986764);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+DROP TABLE IF EXISTS `quiz`;
+CREATE TABLE `quiz` (
+  `QuizID` int NOT NULL,
+  `MuseumID` int DEFAULT NULL,
+  `Explaination` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE `question` (
+  `QuestionID` int NOT NULL,
+  `QuizID` int DEFAULT NULL,
+  `QuestionText` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `option`
+--
+
+DROP TABLE IF EXISTS `option`;
+CREATE TABLE `option` (
+  `OptionID` int NOT NULL,
+  `QuestionID` int DEFAULT NULL,
+  `TEXT` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `isCorrect` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `artifact`
 --
 
+DROP TABLE IF EXISTS `artifact`;
 CREATE TABLE `artifact` (
   `ArtifactID` int NOT NULL,
   `MuseumID` int DEFAULT NULL,
@@ -64,6 +159,7 @@ INSERT INTO `artifact` (`ArtifactID`, `MuseumID`, `ArtifactName`, `Description`,
 -- Table structure for table `museum`
 --
 
+DROP TABLE IF EXISTS `museum`;
 CREATE TABLE `museum` (
   `MuseumID` int NOT NULL,
   `MuseumName` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -88,6 +184,7 @@ INSERT INTO `museum` (`MuseumID`, `MuseumName`, `Address`, `Description`, `Latit
 -- Table structure for table `museum_media`
 --
 
+DROP TABLE IF EXISTS `museum_media`;
 CREATE TABLE `museum_media` (
   `id` int NOT NULL,
   `MuseumId` int NOT NULL,
@@ -119,6 +216,7 @@ INSERT INTO `museum_media` (`id`, `MuseumId`, `file_name`, `mime_type`, `file_pa
 -- Table structure for table `option`
 --
 
+DROP TABLE IF EXISTS `option`;
 CREATE TABLE `option` (
   `OptionID` int NOT NULL,
   `QuestionID` int DEFAULT NULL,
@@ -132,6 +230,7 @@ CREATE TABLE `option` (
 -- Table structure for table `question`
 --
 
+DROP TABLE IF EXISTS `question`;
 CREATE TABLE `question` (
   `QuestionID` int NOT NULL,
   `QuizID` int DEFAULT NULL,
@@ -144,6 +243,7 @@ CREATE TABLE `question` (
 -- Table structure for table `quiz`
 --
 
+DROP TABLE IF EXISTS `quiz`;
 CREATE TABLE `quiz` (
   `QuizID` int NOT NULL,
   `MuseumID` int DEFAULT NULL,
@@ -156,6 +256,7 @@ CREATE TABLE `quiz` (
 -- Table structure for table `useranswer`
 --
 
+DROP TABLE IF EXISTS `useranswer`;
 CREATE TABLE `useranswer` (
   `UserAnswerID` int NOT NULL,
   `QuizID` int DEFAULT NULL,
@@ -171,6 +272,7 @@ CREATE TABLE `useranswer` (
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `UserToken` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
